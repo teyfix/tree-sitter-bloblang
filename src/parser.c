6dd1ff238d609082ea0909e11984a9363a99e7ab
Halil Teyfik <hlltvfk@gmail.com>
2026-05-24 02:47:05 +0300

    chore: bump version to v0.1.5

--- src/parser.c
+++ src/parser.c
@@ -10571,7 +10571,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_bloblang(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 1,
-      .patch_version = 3,
+      .patch_version = 5,
     },
   };
   return &language;
